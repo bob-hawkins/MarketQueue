02ae65daa5969a3a2785236f6c13b63a39cdbba2
bob-hawkins <kinghawkins61@gmail.com>
2024-06-14 17:16:18 +0300

    Updating the config file

--- app/config/config.php
+++ app/config/config.php
@@ -7,6 +7,6 @@ define("DB_USER", "root");
 define("ROOT", dirname(dirname(__FILE__)));
 define("SMTP_USER", "__SMTP_USER__");
 define("SMTP_PASS", "__SMTP_PASS__");
-
-// define("APP_URL", "http://localhost/MarketQueue");
-define("APP_URL", "https://8175-41-89-99-5.ngrok-free.app/MarketQueue");
+define("INFOBIP_BASE_URL", "__INFO_BIP_BASE_URL__");
+define("INFOBIP_AUTH_TOKEN", "__INFO_BIP_AUTH_TOKEN__");
+define("APP_URL", "http://localhost/MarketQueue");
