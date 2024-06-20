f14aad4e9a9bc59790377930ef0b39a8a5dea2e1
bob-hawkins <kinghawkins61@gmail.com>
2024-06-20 09:41:33 +0300

    Editing config.php

--- app/config/config.php
+++ app/config/config.php
@@ -5,15 +5,10 @@ define("DB_HOST", "localhost");
 define("DB_PASS", "");
 define("DB_USER", "root");
 define("ROOT", dirname(dirname(__FILE__)));
-define("SMTP_USER", "bobteststuffonline@gmail.com");
-define("SMTP_PASS", "erfz sfxc wmbp bwhh");
-
-// Jackie
-// define("INFOBIP_BASE_URL", "https://8g8r51.api.infobip.com/sms/2/text/advanced");
-// define("INFOBIP_AUTH_TOKEN", "e80ea862f671d59065561e43c81f6f12-151b3566-5c9a-41a7-bcff-5d858ed2613c");
+define("SMTP_USER", "___SMTP__USER___");
+define("SMTP_PASS", "___SMTP__PASS___");
+define("INFOBIP_BASE_URL", "__BASE__URL__");
+define("INFOBIP_AUTH_TOKEN", "__AUTH__TOKEN__");
 
 
 define("APP_URL", "http://localhost/MarketQueue");
-define("INFOBIP_BASE_URL", "https://6gkkw5.api.infobip.com/sms/2/text/advanced");
-define("INFOBIP_AUTH_TOKEN", "27df8f4165a6b2e13b5e7c792d0291dc-d131c040-591f-46fb-9d1d-0c114a774e86");
-// define("APP_URL", "https://e728-41-89-99-5.ngrok-free.app/MarketQueue");
